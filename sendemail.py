79c96bdbd9642837f5fe69733b7556f6184a18e9
ValdevCompany <141391313+ValdevCompany@users.noreply.github.com>
2024-05-15 17:12:08 +0600

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -15,7 +15,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'wewqw59@gmail.com'
 app.config['MAIL_PASSWORD'] = 'wwew56'
-app.config['MAIL_DEFAULT_SENDER'] = 'miralibaltabaev59@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'miererewbeweaev59@gmail.com'
 
 
 
